cd115a83d01d467beeb125b8bc93344bc572dd94
Kori <ez.kornelia@gmail.com>
2019-08-30 21:16:38 +0200

    Updating App to use routing and start using sass

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './HomePage.scss'
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    );
+  }
+}
+
+export default HomePage;
