3ca2c74875d639d6ab6eb381c28d61b8fca0ee52
Kori <ez.kornelia@gmail.com>
2019-08-31 17:00:54 +0200

    Adding external logos as they were missing from the artboard and doing initial styling

--- src/components/AvailableFor.jsx
+++ src/components/AvailableFor.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-import { Col, Image, Jumbotron, Row } from 'react-bootstrap'
+import { Col, Image, Jumbotron, Row } from "react-bootstrap";
 import "./AvailableFor.scss";
 import flower1 from "../assets/images/flowers_right_side.svg";
 import flower2 from "../assets/images/flowers_left_side.svg";
+import ios from "../assets/images/ios.png";
+import android from "../assets/images/android.png";
 import phone from "../assets/images/Group_1590/Group_1590.png";
 import desktop from "../assets/images/Group_1584/Group_1584.png";
+import globe from "../assets/images/globe.png";
 
 class AvailableFor extends Component {
   render() {
@@ -14,7 +17,36 @@ class AvailableFor extends Component {
           <h3>Available for</h3>
           <Row>
             <Col>
-              <div className="column">
+              <div className="top-row">
+                <Image
+                  src={ios}
+                  alt="..."
+                  style={{ width: "10%", height: "10%" }}
+                />
+              </div>
+            </Col>
+            <Col>
+              <div className="top-row">
+              <Image
+                src={globe}
+                alt="..."
+                style={{ width: "10%", height: "10%" }}
+              />
+              </div>
+            </Col>
+            <Col>
+              <div className="top-row">
+                <Image
+                  src={android}
+                  alt="..."
+                  style={{ width: "10%", height: "10%" }}
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div id="first-column">
                 <Image src={flower1} alt="..." />
                 <Image src={phone} alt="..." />
               </div>
@@ -23,9 +55,9 @@ class AvailableFor extends Component {
               <Image src={desktop} alt="..." />
             </Col>
             <Col>
-              <div className="column">
-              <Image src={phone} alt="..." />
-              <Image src={flower2} alt="..." />
+              <div id="last-column">
+                <Image src={phone} alt="..." />
+                <Image src={flower2} alt="..." />
               </div>
             </Col>
           </Row>
