83a9d7211e711f568947e4c6daf42dbd90c83a03
Kori <ez.kornelia@gmail.com>
2019-09-01 00:11:37 +0200

    Fixing css and img alt

--- src/components/AboutTheApp.jsx
+++ src/components/AboutTheApp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Col, Image, Jumbotron, Row } from 'react-bootstrap'
+import { Col, Container, Image, Jumbotron, Row } from 'react-bootstrap'
 import one from "../assets/images/onboarding_illustration1.svg";
 import two from "../assets/images/onboarding_illustration 2.svg";
 import three from "../assets/images/onboarding_illustration 3.svg";
@@ -9,26 +9,26 @@ class Intro extends Component {
   render() {
     return (
       <Jumbotron className="about">
-        <div>
-          <h3>ABOUT THE APP</h3>
+        <Container fluid>
+          <h3>About the app</h3>
           <Row>
             <Col>
-              <Image src={one} alt="..." />
+              <Image src={one} alt="person with phone" />
               <p>Keep track of your consumption while staying in a hotel.</p>
             </Col>
             <Col>
-              <Image src={two} alt="..." />
+              <Image src={two} alt="person texting" />
               <p>Check out of your hotel in the app, so you can skip lines.</p>
             </Col>
             <Col>
-              <Image src={three} alt="..." />
+              <Image src={three} alt="person working on laptop" />
               <p>
                 Browse the activities, so you can keep up with your hotels
                 events and the best offers near you!
               </p>
             </Col>
           </Row>
-        </div>
+        </Container>
       </Jumbotron>
     );
   }
