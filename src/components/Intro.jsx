253b509471923d356773370daf58e705bbe46c56
Kori <ez.kornelia@gmail.com>
2019-09-01 00:06:21 +0200

    Updating Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,30 +1,29 @@
-import React, {Component} from 'react';
-import { Button, Image, Jumbotron } from 'react-bootstrap'
-import phones from '../assets/images/Group_1598/Group_1598.png'
-import './Intro.scss'
+import React, { Component } from "react";
+import { Button, Col, Image, Jumbotron, Row } from "react-bootstrap";
+import phones from "../assets/images/Group_1598/Group_1598.png";
+import "./Intro.scss";
 
 class Intro extends Component {
   render() {
     return (
-      <Jumbotron className='intro'>
-        <div className="intro-text-container">
-          <h3>CHECK YOURSELF & HELP YOURSELF</h3>
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
-          </p>
-          <Button>Get started</Button>
-        </div>
-        <div>
-          <Image
-            src={phones}
-            alt="phones"
-          />
-        </div>
+      <Jumbotron className="intro" fluid>
+        <Row>
+          <Col>
+            <h1>CHECK YOURSELF & HELP YOURSELF</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <Button>Get started</Button>
+          </Col>
+          <Col>
+            <Image src={phones} alt="phones" />
+          </Col>
+        </Row>
       </Jumbotron>
-    )
+    );
   }
 }
 
