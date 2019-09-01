4bbf079c2a4aee9464efa3bb7b98eba1c5be7b92
Kori <ez.kornelia@gmail.com>
2019-09-02 00:11:47 +0200

    Add missing icons from lowest component and include more responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import "./Contact.scss";
 import logoWithText from "../assets/images/Group_1597.svg";
+import address from "../assets/images/place.svg";
+import email from "../assets/images/envelope.svg";
+import webpage from "../assets/images/webpage.svg";
+import phone from "../assets/images/phone number.svg";
 
 class Contact extends Component {
   render() {
@@ -12,10 +16,22 @@ class Contact extends Component {
             <Image src={logoWithText} alt="the app logo" />
           </Col>
           <Col>
-            <p>Unnamed Road, London NW1 4RT, England</p>
-            <p>info@hotelapp.co</p>
-            <p>www.hotelapp.co</p>
-            <p>+44 20 1234 5678</p>
+            <Row>
+              <Image src={address} alt="" />
+              <p>Unnamed Road, London NW1 4RT, England</p>
+            </Row>
+            <Row>
+              <Image src={email} alt="" />
+              <p>info@hotelapp.co</p>
+            </Row>
+            <Row>
+              <Image src={webpage} alt="" />
+              <p>www.hotelapp.co</p>
+            </Row>
+            <Row>
+              <Image src={phone} alt="" />
+              <p>+44 20 1234 5678</p>
+            </Row>
           </Col>
         </Row>
         <p>
